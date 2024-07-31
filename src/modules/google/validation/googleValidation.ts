09b5090b85c632c05d04c23e3055f826a648e26c
Josue Byiringiro <josue.byiringiro@skrey-software.com>
2024-07-31 20:43:23 +0200

    resolved the conflicts

--- src/modules/google/validation/googleValidation.ts
+++ src/modules/google/validation/googleValidation.ts
@@ -21,7 +21,7 @@ const refreshAccessTokenSchema = Joi.object({
   })
 });
 
-const createEventschema = Joi.object({
+const createEventSchema = Joi.object({
   calendarId: Joi.string().messages({
     'any.required': 'calendarId is required',
     'string.base': 'calendarId must be a string',
@@ -127,7 +127,7 @@ const createEventschema = Joi.object({
   }),
 });
 
-const listEventschema = Joi.object({
+const listEventsSchema = Joi.object({
   calendarId: Joi.string().required().messages({
     'any.required': 'calendarId is required',
     'string.base': 'calendarId must be a string',
@@ -151,4 +151,76 @@ const listEventschema = Joi.object({
   }),
 });
 
-export { codeSchema, refreshAccessTokenSchema, createEventschema, listEventschema };
+const listSpreadSheetsSchema =  Joi.object({
+  pageSize: Joi.string().required().messages({
+    'any.required': 'pageSize is required',
+    'string.base': 'pageSize must be a string',
+    'string.empty': 'pageSize is is not allowed to be empty',
+  }),
+  nextPageToken: Joi.string().messages({
+    'any.required': 'nextPageToken is required and is event name',
+    'string.base': 'nextPageToken must be a string and is event name',
+    'date.empty': 'nextPageToken is not allowed to be empty and is event name',
+  }),
+});
+
+const spreadSheetHeaderSchema = Joi.array().items(Joi.string().required()).min(1).required().messages({
+  'any.required': 'First array for headers is required ex: ["Name", "Email", "etc..."].',
+  'array.base': 'First array must be an array of headers.',
+  'array.min': 'First array of headers must contain at least one value ["Name"].',
+  'string.base': 'Each header item must be a string.',
+  'string.empty': 'First array for headers cannot be empty.'
+});
+
+const spreadSheetDataSchema = Joi.array().items(Joi.string().required()).min(1).required().messages({
+  'any.required': 'Next arrays for data values is required ex: ["Joe", "email@yahoo.com", "etc..."].',
+  'array.base': 'Next arrays must be an array of data values.',
+  'array.min': 'Next arrays of data values must contain at least one value ["Jose"].',
+  'string.empty': 'Next arrays for data values cannot be empty.'
+});
+    
+const createSpreadSheetDataSchema = Joi.object({
+  range: Joi.string().required().messages({
+    'any.required': 'range is required ex: Sheet1!A1:D10',
+    'string.base': 'range must be a string ex: Sheet1!A1:D10',
+    'string.empty': 'range is is not allowed to be empty ex: Sheet1!A1:D10',
+  }),
+  values: Joi.array().items(spreadSheetHeaderSchema, spreadSheetDataSchema).min(2).required().messages({
+    'array.base': 'The "values" must be an array of arrays.',
+    'array.min': 'The "values" array must contain at least two arrays.',
+    'any.required': 'The "values" field is required.',
+    'array.includesRequiredUnknowns': 'The values array must contain at least one header array ex: ["Name", "Email", "etc..."]. followed by at least one data values array ex: ["Joe", "email@yahoo.com", "etc..."].',
+  }),
+});
+
+const rangeSpreadSheetDataSchema =  Joi.object({
+  range: Joi.string().required().messages({
+    'any.required': 'range is required ex: Sheet1!A1:D10',
+    'string.base': 'range must be a string ex: Sheet1!A1:D10',
+    'string.empty': 'range is is not allowed to be empty ex: Sheet1!A1:D10',
+  }),
+});
+
+const updateSpreadSheetDataSchema = Joi.object({
+  range: Joi.string().required().messages({
+    'any.required': 'range is required ex: Sheet1!A1:D10',
+    'string.base': 'range must be a string ex: Sheet1!A1:D10',
+    'string.empty': 'range is is not allowed to be empty ex: Sheet1!A1:D10',
+  }),
+  values: Joi.array().items().min(1).required().messages({
+    'any.required': 'values field is required.',
+    'array.base': 'values must be an array of arrays.',
+    'array.min': 'values array must contain at least 1 array.'
+  }),
+});
+
+export {
+  codeSchema,
+  refreshAccessTokenSchema,
+  createEventSchema,
+  listEventsSchema,
+  listSpreadSheetsSchema,
+  createSpreadSheetDataSchema,
+  rangeSpreadSheetDataSchema,
+  updateSpreadSheetDataSchema
+};
